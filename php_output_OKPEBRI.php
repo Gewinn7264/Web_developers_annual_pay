98f212cbd91bc2abdfd4069957e7fd65ecdc6655
Okpebri Samuel <133010120+Gewinn7264@users.noreply.github.com>
2023-05-28 21:29:29 +0100

    Add files via upload

---
+++ php_output_OKPEBRI.php
@@ -0,0 +1,10 @@
+<?php
+$name = "<h1>OKPEBRI SAMUEL INYANG</h1>";
+$regno = "2021/HND/34357/CS<br>";
+$state_of_origin = "CROSS RIVER STATE<br>";
+$project = "<strong>WEB BASED LOGBOOK</strong>";
+echo $name;
+echo $regno;
+echo $state_of_origin;
+echo $project;
+?>
